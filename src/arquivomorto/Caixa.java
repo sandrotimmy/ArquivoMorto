7b0a5d388d2b67ffd24a46f2ef8b7c9ba548011b
sandrotimmy <smachado.ti@gmail.com>
2015-12-21 08:37:48 -0200

    21/12/2015

--- src/arquivomorto/Caixa.java
+++ src/arquivomorto/Caixa.java
@@ -1,53 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package arquivomorto;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-
-
-/**
- *
- * @author Administrador
- */
-
 @Entity
 public class Caixa {
 
     @Id
-    public int idCaixa;
-    public String conteudoCaixa;
+    private Long Id;
+    private String conteudoCaixa;
 
-    public Caixa(int idCaixa, String conteudoCaixa) {
-        this.idCaixa = idCaixa;
-        this.conteudoCaixa = conteudoCaixa;
-    }
-    
-    public Caixa() {
+    public Long getId() {
+        return Id;
     }
 
-    public int getidCaixa() {
-        return idCaixa;
+    public void setId(Long Id) {
+        this.Id = Id;
     }
 
-    public String getconteudoCaixa() {
+    public String getConteudoCaixa() {
         return conteudoCaixa;
     }
 
-    public void setidCaixa(int idCaixa) {
-        this.idCaixa = idCaixa;
-    }
-
-    public void setconteudoCaixa(String conteudoCaixa) {
+    public void setConteudoCaixa(String conteudoCaixa) {
         this.conteudoCaixa = conteudoCaixa;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString(); //To change body of generated methods, choose Tools | Templates.
     }
 }
